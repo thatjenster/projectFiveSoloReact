4b729b6a204880711fe1c279b57b8be26640f041
Jenny Dinh <jennythi.d@gmail.com>
2020-06-24 01:05:21 -0400

    Updates

---
+++ precache-manifest.662632e1bd36dc68798487ae0ff5c766.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12d5649426f053521eb04d28ed327ed8",
+    "url": "/projectFiveSoloReact/index.html"
+  },
+  {
+    "revision": "b05161f3f252ec2d5b17",
+    "url": "/projectFiveSoloReact/static/css/2.9af23e3e.chunk.css"
+  },
+  {
+    "revision": "4a6bcd47dbeb4fee4910",
+    "url": "/projectFiveSoloReact/static/css/main.bdbb4569.chunk.css"
+  },
+  {
+    "revision": "b05161f3f252ec2d5b17",
+    "url": "/projectFiveSoloReact/static/js/2.5909b1d3.chunk.js"
+  },
+  {
+    "revision": "f95aa2d42737df318d1594d096635f08",
+    "url": "/projectFiveSoloReact/static/js/2.5909b1d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a6bcd47dbeb4fee4910",
+    "url": "/projectFiveSoloReact/static/js/main.e47f8c6f.chunk.js"
+  },
+  {
+    "revision": "d4529ad16149cccc64ca",
+    "url": "/projectFiveSoloReact/static/js/runtime-main.788f95ef.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/projectFiveSoloReact/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/projectFiveSoloReact/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/projectFiveSoloReact/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/projectFiveSoloReact/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/projectFiveSoloReact/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/projectFiveSoloReact/precache-manifest.662632e1bd36dc68798487ae0ff5c766.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/projectFiveSoloReact/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
