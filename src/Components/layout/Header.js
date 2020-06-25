0ef9c2a631d6a8f80190eca33aff6d62189f9128
Jenny Dinh <jennythi.d@gmail.com>
2020-06-25 08:44:02 -0400

    final final commit

--- src/Components/layout/Header.js
+++ src/Components/layout/Header.js
@@ -5,9 +5,9 @@ const Header = () => {
     function displayInfo(e){
         Swal.fire({
             title: 'How to use:',
-            html: '1. Sign up with any email address to Begin.' + ' <br/><br/>' + '2. Don\'t worry.. you don\'t need to use a real e-mail address...' + ' <br/><br/>' + '3. Once Signed in, Enter a Travel Memory and see it plot on the map!',
+            html: '1. Sign up with any email address to Begin. (DON\'T WORRY... you don\'t need to use a real e-mail address, just an e-mail in correct format).' + ' <br/><br/>' + '2. This site does not send any spam emails &#128523;' + ' <br/><br/>' + '3. Once Signed in, Enter a Travel Memory and see it plot on the map!',
             showConfirmButton: false,
-            timer: 4000,
+            timer: 5500,
             showClass: {popup: 'animate__animated animate__bounce'},
             showCloseButton: true,
             showCloseButtonColor: 'pink'
@@ -19,7 +19,11 @@ const Header = () => {
         <div id="homePage" className="wrapper">
             <div className="photo">
             <img className="headerPhoto" src="https://picsum.photos/640/360" ></img>
-            <div className="titleHeader">Memories: A Personal Travel Diary<span className="infoBox" onClick={displayInfo}>&#x24D8;</span></div>
+            <div className="titleHeader">Memories: A Personal Travel Diary
+                <span className="infoBox" onClick={displayInfo}>
+                    <a>&#x24D8;</a>
+                </span>
+            </div>
             </div>
         </div>
     )
