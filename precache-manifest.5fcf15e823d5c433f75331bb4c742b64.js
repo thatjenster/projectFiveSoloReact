11b625da87c44a3460d11838d8627e961cc19180
Jenny Dinh <jennythi.d@gmail.com>
2020-07-28 11:08:48 -0400

    Updates

---
+++ precache-manifest.5fcf15e823d5c433f75331bb4c742b64.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d47373072322f1c3d53b38428623c700",
+    "url": "/projectFiveSoloReact/index.html"
+  },
+  {
+    "revision": "b098e8ac5ccc2f23f1af",
+    "url": "/projectFiveSoloReact/static/css/2.9af23e3e.chunk.css"
+  },
+  {
+    "revision": "1b4a335c1749617f2754",
+    "url": "/projectFiveSoloReact/static/css/main.eac467ef.chunk.css"
+  },
+  {
+    "revision": "b098e8ac5ccc2f23f1af",
+    "url": "/projectFiveSoloReact/static/js/2.04d9d0cf.chunk.js"
+  },
+  {
+    "revision": "f95aa2d42737df318d1594d096635f08",
+    "url": "/projectFiveSoloReact/static/js/2.04d9d0cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b4a335c1749617f2754",
+    "url": "/projectFiveSoloReact/static/js/main.05bc022a.chunk.js"
+  },
+  {
+    "revision": "d4529ad16149cccc64ca",
+    "url": "/projectFiveSoloReact/static/js/runtime-main.788f95ef.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/projectFiveSoloReact/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/projectFiveSoloReact/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/projectFiveSoloReact/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/projectFiveSoloReact/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/projectFiveSoloReact/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "ecdad0c23e327a86e1076262b808a191",
+    "url": "/projectFiveSoloReact/static/media/markerpin.ecdad0c2.png"
+  },
+  {
+    "revision": "f9898293baf8d486e770165446094d84",
+    "url": "/projectFiveSoloReact/static/media/mountfuji.f9898293.png"
+  }
+]);
